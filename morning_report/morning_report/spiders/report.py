15656c0478d7038a45ad65c628fa5743f890af59
Oleg <pawlik806@gmail.com>
2020-07-20 12:54:22 +0300

    remove some feat

--- morning_report/morning_report/spiders/report.py
+++ morning_report/morning_report/spiders/report.py
@@ -33,28 +33,12 @@ class ReportSpider(scrapy.Spider):
         buy = response.xpath(
             """//li[@class="pair__block"][1]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-num')]/text()"""
         ).extract_first()
-        move_buy = response.xpath(
-            """//li[@class="pair__block"][1]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-move')]/span/text()"""
-        ).extract_first()
-
+        
         sell = response.xpath(
             """//li[@class="pair__block"][2]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-num')]/text()"""
         ).extract_first()
-        move_sell = response.xpath(
-            """//li[@class="pair__block"][2]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-move')]/span/text()"""
-        ).extract_first()
-
-        try:
-            move_buy = int(move_buy)
-        except:
-            move_buy = 0
-
-        try:
-            move_sell = int(move_sell)
-        except:
-            move_sell = 0
-
-        result = "$$$%0AПокупка: {buy}%0AИзменение за день: {move_buy}%0AПродажа: {sell}%0AИзменение за день: {move_sell}".format(buy=buy, move_buy=move_buy, sell=sell, move_sell=move_sell)
+        
+        result = "$$$\nAПокупка: {buy}\nПродажа: {sell}".format(buy=buy, move_buy=move_buy, sell=sell, move_sell=move_sell)
         result = urllib.quote(result)
         yield Request(self.url_to_send_message.format(result), dont_filter=True, callback=self.ok)
 
