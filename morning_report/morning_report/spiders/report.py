ef4bc1d4683d9d3817662e10f7c943ce56fdba2c
Oleg <pawlik806@gmail.com>
2020-07-31 16:12:25 +0300

    feat

--- morning_report/morning_report/spiders/report.py
+++ morning_report/morning_report/spiders/report.py
@@ -7,26 +7,38 @@ import urllib
 class ReportSpider(scrapy.Spider):
     name = 'report'
     allowed_domains = ['']
-    url_to_send_message = "https://api.telegram.org/bot1149905775:AAH3E0Mvhi9d1itnCBSNY85Il5TLZqhijYU/sendMessage?chat_id=342491940&text={}"
+    # url_to_send_message = "https://api.telegram.org/bot1149905775:AAH3E0Mvhi9d1itnCBSNY85Il5TLZqhijYU/sendMessage?chat_id=342491940&text={}" # tuchka
+    url_to_send_message = "https://api.telegram.org/bot1149905775:AAH3E0Mvhi9d1itnCBSNY85Il5TLZqhijYU/sendMessage?chat_id=211507050&text={}"  # I'm
 
     def start_requests(self):
-        urls = ["https://nibulon.com/data/zakupivlya-silgospprodukcii/zakupivelni-cini.html",
-                "https://obmenka.kharkov.ua/usd-uah"]
-        body = {"culture": "101000000000001", "bazis": "47834", "priceDate": "5a0ea51b6667f940de3c4072202976b6"}
+        urls = [
+            "https://nibulon.com/data/zakupivlya-silgospprodukcii/zakupivelni-cini.html",
+            "https://obmenka.kharkov.ua/usd-uah",
+            "https://obmenka.kharkov.ua/eur-uah",
+            "https://prometey.org.ua/zakupochny-e-tseny-na-e-levatorah/?_sft_culture=yachmin",
+        ]
 
         for url in urls:
             if "nibulon" in url:
-                yield FormRequest(url, formdata=body, callback=self.parse_price)
-            else:
+                body = {"culture": "101000000000001", "bazis": "47834",
+                        "priceDate": "5a0ea51b6667f940de3c4072202976b6"}
+                yield FormRequest(url, formdata=body, callback=self.parse_nibulon)
+            elif "prometey" in url:
+                yield Request(url, callback=self.parse_prometey)
+            elif "usd-uah" in url:
                 yield Request(url, callback=self.parse_usd)
+            elif "eur-uah" in url:
+                yield Request(url, callback=self.parse_eur)
 
-    def parse_price(self, response):
+    def parse_nibulon(self, response):
 
-        price = response.xpath('//div[@class="culture_head collapsed"]/descendant::text()').extract()
+        price = response.xpath(
+            '//div[@class="culture_head collapsed"]/descendant::text()').extract()
         clean_price = ''.join([i.strip() for i in price if i])
-        clean_price = clean_price.encode("UTF-8")
-        # clean_price = urllib.quote(clean_price)
-        yield Request(self.url_to_send_message.format(clean_price), dont_filter=True, callback=self.ok)
+        result = "Nibulon:\n"+clean_price
+        # result = clean_price.encode("UTF-8")
+
+        yield Request(self.url_to_send_message.format(result), dont_filter=True, callback=self.ok)
 
     def parse_usd(self, response):
 
@@ -34,13 +46,39 @@ class ReportSpider(scrapy.Spider):
         buy = response.xpath(
             """//li[@class="pair__block"][1]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-num')]/text()"""
         ).extract_first()
-        
+
         sell = response.xpath(
             """//li[@class="pair__block"][2]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-num')]/text()"""
         ).extract_first()
-        
-        result = "$$$\nПокупка: {buy}\nПродажа: {sell}".format(buy=buy, sell=sell)
+
+        result = "UAH-USD\nПокупка: {buy}\nПродажа: {sell}".format(
+            buy=buy, sell=sell)
         result = urllib.quote(result)
+        yield Request(self.url_to_send_message.format(result), dont_filter=True, callback=self.ok)
+
+    def parse_eur(self, response):
+
+        # uah-eur
+        buy = response.xpath(
+            """//li[@class="pair__block"][1]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-num')]/text()"""
+        ).extract_first()
+
+        sell = response.xpath(
+            """//li[@class="pair__block"][2]//div[contains(@class, 'block-retail')]/div[contains(@class, 'block-num')]/text()"""
+        ).extract_first()
+
+        result = "UAH-EUR\nПокупка: {buy}\nПродажа: {sell}".format(
+            buy=buy, sell=sell)
+        result = urllib.quote(result)
+        yield Request(self.url_to_send_message.format(result), dont_filter=True, callback=self.ok)
+
+    def parse_prometey(self, response):
+
+        price = "".join(response.xpath(
+            """//div[@class="priceblock"][1]//div[contains(@class, "coin")][last()]//div[contains(@class,"coin_title")]/text()"""
+        ).extract())
+        result = "Prometey:\n" + price.split(",")[0].strip()
+
         yield Request(self.url_to_send_message.format(result), dont_filter=True, callback=self.ok)
 
     def ok(self, response):
